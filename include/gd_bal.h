0ad47c5414f515bb8d91daf6bfcf3461450b0e04
Owen Shepherd <owen.shepherd@e43.eu>
2014-07-13 21:08:29 +0100

    Add macros for generating an "ioctl map"; simplify implementing ioctls. Change gd_ioctl_gen output format

--- include/gd_bal.h
+++ include/gd_bal.h
@@ -16,6 +16,7 @@
 #ifndef GD_BAL_H
 #define GD_BAL_H
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -28,5 +29,41 @@
 typedef struct gd_device {
     int (*ioctl)(struct gd_device *, unsigned, ...);
 } *gd_device_t;
+
+int gd_ioctl(gd_device_t, unsigned, ...);
+
+#include "gd_ioctl.inc"
+
+#define GD_BEGIN_IOCTL_MAP(_type, _name)                                       \
+static int GD_CONCAT(_name, _ioctl)(gd_device_t dev_, unsigned ioctl, ...)     \
+{                                                                              \
+    int rv = 0;                                                                \
+    _type dev = (_type) dev_;                                                  \
+    va_list ap, *pap = &ap;                                                    \
+    va_start(ap, ioctl);                                                       \
+postForward:                                                                   \
+    switch(ioctl) {                                                            \
+    case GD_FORWARD_IOCTL: {                                                   \
+        ioctl = va_arg(*pap, unsigned);                                        \
+        pap   = va_arg(*pap, va_list *);                                       \
+        goto postForward;                                                      \
+    }
+
+#define GD_END_IOCTL_MAP()                                                     \
+    default:                                                                   \
+        /*errno = EINVAL;*/                                                    \
+        rv = -1;                                                               \
+    }                                                                          \
+    va_end(ap);                                                                \
+    return rv;                                                                 \
+}
+
+#define GD_END_IOCTL_MAP_FORWARD(_forwardTo)                                   \
+    default:                                                                   \
+        rv = (_forwardTo)(dev_, GD_FORWARD_IOCTL, ioctl, pap);                 \
+    }                                                                          \
+    va_end(ap);                                                                \
+    return rv;                                                                 \
+}
 
 #endif
