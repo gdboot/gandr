14f9fa3e4e993856e589809c0881caacecaa0440
Shikhin Sethi <draumr.shikhin@gmail.com>
2014-09-27 04:03:26 +0530

    [bal] Added mmap handling code.

---
+++ bal/mmap.c
@@ -0,0 +1,129 @@
+/* Copyright © 2014, Shikhin Sethi
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any 
+ * purpose with or without fee is hereby granted, provided that the above 
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH 
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY 
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, 
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR 
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR 
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <gd_bal.h>
+#include <string.h>
+
+static gd_memory_type overlap_precedence[] = {
+    /*! The overlapping region is promoted to the one that comes earlier. */
+    EfiUnusableMemory, EfiPalCode, EfiMemoryMappedIOPortSpace, EfiMemoryMappedIO,
+    EfiACPIMemoryNVS, EfiRuntimeServicesCode, EfiRuntimeServicesData,
+    EfiACPIReclaimMemory, EfiLoaderCode, EfiLoaderData, EfiBootServicesCode,
+    EfiBootServicesData, EfiConventionalMemory, EfiReservedMemoryType
+};
+
+/*! Returns the type with higher precedence. */
+static gd_memory_type higher_precedence(gd_memory_type a, gd_memory_type b)
+{
+    for (size_t i = 0; i < sizeof overlap_precedence/sizeof (gd_memory_type); i++)
+    {
+        if (overlap_precedence[i] == a) 
+            return a;
+        else if (overlap_precedence[i] == b)
+            return b;
+    }
+
+    // Unusable.
+    return EfiUnusableMemory;
+}
+
+static void merge_adjacent(gd_memory_map_table *table, size_t first_idx)
+{
+    if ((table->entries[first_idx].physical_start + table->entries[first_idx].size 
+        == table->entries[first_idx + 1].physical_start) &&
+        (table->entries[first_idx].type == table->entries[first_idx + 1].type)) {
+        table->entries[first_idx].size += table->entries[first_idx + 1].size;
+        table->entries[first_idx].attributes |= table->entries[first_idx + 1].attributes;
+        mmap_remove_entry(table, first_idx + 1);
+    }
+}
+
+static void fix_overlap(gd_memory_map_table *table,
+                        size_t first_idx)
+{
+    gd_memory_map_entry *first = &table->entries[first_idx];
+    gd_memory_map_entry *second = &table->entries[first_idx + 1];
+
+    if ((first->physical_start + first->size) <= second->physical_start)
+        return;
+
+    gd_memory_map_entry new_entry;
+
+    // Partition in three, where the new_entry is the right-most part.
+    uint64_t first_physical_end = first->physical_start + first->size;
+    uint64_t second_physical_end = second->physical_start + second->size;
+    if (first_physical_end < second_physical_end) {
+        new_entry.physical_start = first_physical_end;
+        new_entry.size = second_physical_end - first_physical_end;
+        new_entry.type = second->type; new_entry.attributes = second->attributes;
+    } else {
+        new_entry.physical_start = second_physical_end;
+        new_entry.size = first_physical_end - second_physical_end;
+        new_entry.type = first->type; new_entry.attributes = first->attributes;
+    }
+
+    first->size = second->physical_start - first->physical_start;
+    second->size = new_entry.physical_start - second->physical_start;
+    /* TODO: handle attributes. */
+    second->attributes = first->attributes | second->attributes;
+    second->type = higher_precedence(first->type, second->type);
+
+    if (!second->size) {
+        mmap_remove_entry(table, first_idx + 1);
+    }
+    if (!first->size) {
+        mmap_remove_entry(table, first_idx);
+    }
+
+    mmap_add_entry(table, new_entry);
+}
+
+void mmap_remove_entry(gd_memory_map_table *table, size_t idx)
+{
+    for(size_t i = idx;
+        i < ((table->header.length - sizeof (gd_table))/sizeof (gd_memory_map_entry) - 1); i++) {
+        memcpy(&table->entries[i], &table->entries[i + 1], sizeof (gd_memory_map_entry));
+    }
+    table->header.length -= sizeof (gd_memory_map_entry);
+}
+
+void mmap_add_entry(gd_memory_map_table *table, gd_memory_map_entry entry)
+{
+    if (!entry.size)
+        return;
+
+    size_t idx;
+    for (idx = (table->header.length - sizeof (gd_table))/sizeof (gd_memory_map_entry);
+         idx > 0; idx--) {
+        // Sort in increasing order.
+        if (table->entries[idx - 1].physical_start <= entry.physical_start)
+            break;
+
+        memcpy(&table->entries[idx], &table->entries[idx - 1], sizeof (gd_memory_map_entry));
+    }
+
+    table->header.length += sizeof (gd_memory_map_entry);
+    memcpy(&table->entries[idx], &entry, sizeof (gd_memory_map_entry));
+
+    /* TODO: look after attributes. */
+    if (idx) {
+        fix_overlap(table, idx - 1);
+        merge_adjacent(table, idx - 1);
+    }
+    if (idx < ((table->header.length - sizeof (gd_table))/sizeof (gd_memory_map_entry) - 1)) {
+        fix_overlap(table, idx);
+        merge_adjacent(table, idx);
+    }
+}
--- include/gd_bal.h
+++ include/gd_bal.h
@@ -1,4 +1,4 @@
-/* Copyright © 2013, Owen Shepherd
+/* Copyright © 2014, Owen Shepherd & Shikhin Sethi
  * 
  * Permission to use, copy, modify, and/or distribute this software for any 
  * purpose with or without fee is hereby granted, provided that the above 
@@ -19,6 +19,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <limits.h>
 
 #define GD_CONCAT_(a, b) a##b
 #define GD_CONCAT(a, b) GD_CONCAT_(a, b)
@@ -65,5 +66,111 @@ postForward:                                                                   \
     va_end(ap);                                                                \
     return rv;                                                                 \
 }
+
+/*! A Gandr table header.
+ *
+ * All tables are aligned to 8 byte boundaries. When processing multiple
+ * sequential tables, e.g. the boot information directory, the next table is
+ * found at the address
+ *      ((const char*)&table) + (table.length + 7 & ~7)
+ *
+ * You should verify that the size of each table is <b>at least</b> that
+ * specified by the specification. You <b>must</b> accept longer tables,
+ * ignoring their extra content, to facilitate future specification revisions.
+ *
+ * Any table with an identifier starting with a lower case letter is boot
+ * services only and a kernel loader can discard it when preparing the data
+ * to pass to the kernel.
+ */
+typedef struct {
+    /*! Identifier of the table */
+    uint32_t id;
+
+    /*! Length of the table, in bytes, including this header */
+    uint32_t length;
+} gd_table;
+
+#define GD_TABLE_ID(a, b, c, d) \
+    ((uint32_t) ((a << 24) | (b << 16) | (c << 8) | (d)))
+
+/*! Copies the table ID to the passed buffer, in "natural reading order"
+ *  The passed buffer <b>must</b> at least 5 bytes in size, as this function
+ *  null terminates it
+ */
+static inline void gd_table_id_to_string(char *buf, const gd_table *table)
+{
+    buf[0] = (char) (table->id >> 24 & 0xFF);
+    buf[1] = (char) (table->id >> 16 & 0xFF);
+    buf[2] = (char) (table->id >> 8  & 0xFF);
+    buf[3] = (char) (table->id       & 0xFF);
+    buf[4] = 0;
+}
+
+/*! Returns a pointer to the next table */
+static inline gd_table *gd_next_table(const gd_table *table)
+{
+    return (gd_table*) (((char*) table) + ((table->length + 7) & ~7));
+}
+
+/*! Defined types of a memory region.
+ *
+ *  Type codes 0x0000_0000 - 0x7FFF_FFFF are taken from the UEFI specification.
+ *  Type codes 0x8000_0000 - 0xFFFF_FFFF are Gandr defined
+ */
+typedef enum {
+    EfiReservedMemoryType = 0,
+    EfiLoaderCode,
+    EfiLoaderData,
+    EfiBootServicesCode,
+    EfiBootServicesData,
+    EfiRuntimeServicesCode,
+    EfiRuntimeServicesData,
+    EfiConventionalMemory,
+    EfiUnusableMemory,
+    EfiACPIReclaimMemory,
+    EfiACPIMemoryNVS,
+    EfiMemoryMappedIO,
+    EfiMemoryMappedIOPortSpace,
+    EfiPalCode,
+
+    /*! ARM EABI and similar make enum sizes dependent upon their max value
+     *  Force this enum to 32-bits
+     */
+    gd_memory_force_size = INT_MAX
+} gd_memory_type;
+
+/*! A memory map entry. This format is aligned with, but not the same as that
+ *  used by the UEFI specification. In particular, UEFI uses a pageCount member;
+ *  Gandr uses a size member;
+ */
+typedef struct {
+    /*! The type of the memory region. */
+    gd_memory_type type;
+
+    /*! Physical address at which the region begins */
+    uint64_t physical_start;
+
+    /*! Virtual address at which the loader mapped this region, if it was mapped */
+    uint64_t virtual_start;
+
+    /*! Size, in bytes, of the region */
+    uint64_t size;
+
+    /*! Attribute bits
+     *  \see gd_memory_map_attribute
+     */
+    uint64_t attributes;
+} gd_memory_map_entry;
+
+/*! System memory map
+ */
+typedef struct {
+    gd_table header;
+    gd_memory_map_entry entries[];
+} gd_memory_map_table;
+#define GD_MEMORY_MAP_TABLE_ID GD_TABLE_ID('M', 'M', 'A', 'P')
+
+void mmap_remove_entry(gd_memory_map_table *table, size_t idx);
+void mmap_add_entry(gd_memory_map_table *table, gd_memory_map_entry entry);
 
 #endif
