9f6858b14a8e18bc55c16e2e71f3a25ef9a8e9e6
Shikhin Sethi <draumr.shikhin@gmail.com>
2014-09-27 04:06:47 +0530

    Fix copyright year range.

--- include/gd_bal.h
+++ include/gd_bal.h
@@ -1,4 +1,4 @@
-/* Copyright © 2014, Owen Shepherd & Shikhin Sethi
+/* Copyright © 2013-2014, Owen Shepherd & Shikhin Sethi
  * 
  * Permission to use, copy, modify, and/or distribute this software for any 
  * purpose with or without fee is hereby granted, provided that the above 
