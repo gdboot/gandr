56cd427704b812b459c2d0f16e80db1d876bdbc2
Shikhin Sethi <draumr.shikhin@gmail.com>
2015-03-30 23:49:08 +0530

    [bios bal] Add VBE modes to list.

--- bal/platform/bios/vbe.c
+++ bal/platform/bios/vbe.c
@@ -15,10 +15,55 @@
 
 #include <bal/bios_services.h>
 #include <bal/vbe.h>
+#include <gd_queue.h>
+#include <stdlib.h>
+#include <string.h>
 
 volatile struct vbe_info_block vbe_info_block;
 
-void vbe_init();
+typedef struct vbe_mode_list_entry {
+    SLIST_ENTRY(vbe_mode_list_entry)    node;
+    uint32_t                            mode_identifier;
+    struct vbe_mode_info                entry;
+} vbe_mode_list_entry;
+
+static SLIST_HEAD(vbe_mode_list, vbe_mode_list_entry) modes = SLIST_HEAD_INITIALIZER(&modes);
+
+vbe_mode_list_entry vga_text_modes[] = {
+    {
+        /* 80x25 text mode */
+        .mode_identifier = 0x03,
+        .entry = {
+            .attributes = VBE_MODE_ATTR_SUPPORTED |
+                          VBE_MODE_ATTR_EXTENDED_INFORMATION_AVAILABLE |
+                          VBE_MODE_ATTR_TTY_BIOS_SUPPORT,
+            .window_a_attr = VBE_WINDOW_ATTR_RELOCATABLE |
+                             VBE_WINDOW_ATTR_READABLE |
+                             VBE_WINDOW_ATTR_WRITEABLE,
+            .window_b_attr = 0,
+            .window_granularity = 16,       /* In KiB. */
+            .window_size = 16,              /* In KiB. */
+            .window_a_seg = 0xB800,
+            .window_position_func = 0,
+            .bytes_per_scanline = 80 * 2,
+
+            .width = 80,
+            .height = 25,
+            .width_of_char_cell = 9,        /* In pixels. */
+            .height_of_char_cell = 16,      /* In pixels. */
+            .memory_planes = 1,
+            .bpp = 16,
+            .banks = 1,
+            .memory_model = TEXT_MODE,
+            .size_of_bank = 0,
+            .image_pages = 8 - 1,
+            .reserved_0 = 1,
+
+            .banked_num_images = 8 - 1
+        }
+    },
+};
+
 void vbe_init()
 {
     // Check for valid video card.
@@ -30,6 +75,7 @@ void vbe_init()
         return;
 
     // Add VGA text modes to the mode list.
+    SLIST_INSERT_HEAD(&modes, &vga_text_modes[0], node);
 
     regs.eax = 0x4F00;
     regs.es = rm_seg_from_ptr((const void*)&vbe_info_block);
@@ -52,4 +98,32 @@ void vbe_init()
         return;
 
     // Add VBE modes to the mode list.
+    for (uint16_t *mode_identifiers = rm_ptr_from_far(vbe_info_block.video_modes_ptr);
+         *mode_identifiers != 0xFFFF; mode_identifiers++) {
+        // Get mode information.
+        regs.eax = 0x4F01; regs.ecx = *mode_identifiers;
+        regs.es = rm_seg_from_ptr((const void*)&scratch_buffer);
+        regs.edi = rm_offset_from_ptr((const void*)&scratch_buffer);
+
+        bios_int_call(0x10, &regs);
+
+        if ((regs.eax & 0xFF) != 0x4F) /* Function not supported. */
+            return;
+        else if ((regs.eax >> 8) & 0xFF) /* Unsuccessful. */
+            continue;
+
+        if (!(((struct vbe_mode_info*)&scratch_buffer)->attributes & VBE_MODE_ATTR_SUPPORTED))
+            continue;
+
+        vbe_mode_list_entry *new_entry = malloc(sizeof *new_entry);
+        if (!new_entry) {
+            // TODO: Show warning.
+            return;
+        }
+
+        // Add to list.
+        new_entry->mode_identifier = *mode_identifiers;
+        memcpy(&new_entry->entry, &scratch_buffer, sizeof (struct vbe_mode_info));
+        SLIST_INSERT_HEAD(&modes, new_entry, node);
+    }
 }
--- include/platform/bios/bal/vbe.h
+++ include/platform/bios/bal/vbe.h
@@ -35,4 +35,96 @@ struct vbe_info_block {
     uint8_t oem_data[256];
 } __attribute__((packed));
 
+struct vbe_mode_info {
+    uint16_t attributes;
+    uint8_t window_a_attr;
+    uint8_t window_b_attr;
+    uint16_t window_granularity;    /* In KiB. */
+    uint16_t window_size;           /* In KiB. */
+    uint16_t window_a_seg;
+    uint16_t window_b_seg;
+    uint32_t window_position_func;  /* Equivalent to AX=4F05h, int 0x10. */
+    uint16_t bytes_per_scanline;
+
+    /* Optional for VESA modes in v1.0/1.1. */
+    uint16_t width;                 /* In pixels (graphics) or characters (text). */
+    uint16_t height;                /* In pixels (graphics) or characters (text). */
+    uint8_t width_of_char_cell;     /* In pixels. */
+    uint8_t height_of_char_cell;    /* In pixels. */
+    uint8_t memory_planes;
+    uint8_t bpp;
+    uint8_t banks;
+    uint8_t memory_model;
+    uint8_t size_of_bank;           /* In KiB. */
+    uint8_t image_pages;
+    uint8_t reserved_0;             /* 0x00 for VBE 1.0-2.0, 0x01 for VBE 3.0. */
+
+    /* VBE v1.2+. */
+    uint8_t red_mask_size;
+    uint8_t red_field_pos;
+    uint8_t green_mask_size;
+    uint8_t green_field_pos;
+    uint8_t blue_mask_size;
+    uint8_t blue_field_pos;
+    uint8_t reserved_mask_size;
+    uint8_t reserved_field_position;
+    uint8_t direct_color_attr;
+
+    /* VBE 2.0+. */
+    uint32_t lfb_address;
+    uint32_t reserved_1;
+    uint16_t reserved_2;
+
+    /* VBE 3.0+. */
+    uint16_t lin_bytes_per_scanline;
+    uint8_t banked_num_images;
+    uint8_t lin_num_images;
+    uint8_t lin_red_mask_size;
+    uint8_t lin_red_field_pos;
+    uint8_t lin_green_mask_size;
+    uint8_t lin_green_field_pos;
+    uint8_t lin_blue_mask_size;
+    uint8_t lin_blue_field_pos;
+    uint8_t lin_reserved_mask_size;
+    uint8_t lin_reserved_field_pos;
+    uint32_t max_pixel_clock;
+
+    uint8_t reserved_3[190];
+} __attribute__((packed));
+
+enum vbe_mode_attributes {
+    VBE_MODE_ATTR_SUPPORTED                      = (1 << 0),
+    VBE_MODE_ATTR_EXTENDED_INFORMATION_AVAILABLE = (1 << 1),
+    VBE_MODE_ATTR_TTY_BIOS_SUPPORT               = (1 << 2),
+    VBE_MODE_ATTR_COLOR_MODE                     = (1 << 3),
+    VBE_MODE_ATTR_GRAPHICS_MODE                  = (1 << 4),
+    VBE_MODE_ATTR_NOT_VGA_COMPATIBLE             = (1 << 5),
+    VBE_MODE_ATTR_NO_VGA_COMPATIBLE_WINDOW       = (1 << 6),
+    VBE_MODE_ATTR_LINEAR_FRAME_BUFFER_MODE       = (1 << 7),
+    VBE_MODE_ATTR_DOUBLE_SCAN_MODE               = (1 << 8),
+    VBE_MODE_ATTR_INTERLACE_MODE                 = (1 << 9),
+    VBE_MODE_ATTR_HARDWARE_TRIPLE_BUFFER         = (1 << 10),
+    VBE_MODE_ATTR_HARDWARE_STEREOSCOPIC_DISPLAY  = (1 << 11),
+    VBE_MODE_ATTR_DUAL_DISPLAY_START_ADDRESS     = (1 << 12)
+};
+
+enum vbe_window_attributes {
+    VBE_WINDOW_ATTR_RELOCATABLE = (1 << 1),
+    VBE_WINDOW_ATTR_READABLE    = (1 << 2),
+    VBE_WINDOW_ATTR_WRITEABLE   = (1 << 3)
+};
+
+enum vbe_memory_model {
+    TEXT_MODE = 0,
+    CGA_GRAPHICS,
+    HERCULES_GRAPHICS,
+    PLANAR,
+    PACKED_PIXEL,
+    NON_CHAIN_256_COLOR,
+    DIRECT_COLOR,
+    YUV
+};
+
+void vbe_init();
+
 #endif
